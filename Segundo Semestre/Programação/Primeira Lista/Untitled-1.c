24284866881ab504e37d043af99e98d903181cd5
leonan <leonan.marques886@gmail.com>
2022-09-10 15:08:02 -0300

    quarta questão terminada

--- Segundo Semestre/Programação/Primeira Lista/Untitled-1.c
+++ Segundo Semestre/Programação/Primeira Lista/Untitled-1.c
@@ -31,58 +31,64 @@ int main(void) {
     printf("O período decorrido foi de %2.1d horas, %2.1d minutos e %5.2f segundos\n", horas, minutos, segundos);*/
 
     //Quarta Questao
-    int quantia, qtdCem, qtdCinq, qtdVint, qtdDez, qtdCinc, qtdDois, qtdUm;
+    int quantia;
+    int qtdCem = 0;
+    int qtdVint = 0;
+    int qtdCinc = 0;
+    int qtdDois = 0;
+    int qtdUm = 0;
+    int qtdDez = 0;
+    int qtdCinq = 0;
     printf("Digite o valor: \n");
     scanf("%d", &quantia);
-    while (quantia > 0) {
-        if (quantia > 100){
-            qtdCem = 0;
-            while (quantia > 100) {
-                quantia -= 100;
-                qtdCem++;
-            }
+
+    if (quantia > 100){
+        qtdCem = 0;
+        while (quantia >= 100) {
+            quantia -= 100;
+            qtdCem++;
         }
-        if (quantia > 50){
-            qtdCinq = 0;
-            while (quantia > 50) {
-                quantia -= 50;
-                qtdCinq++;
-            }
+    }
+    if (quantia >= 50){
+        qtdCinq = 0;
+        while (quantia >= 50) {
+            quantia -= 50;
+            qtdCinq++;
         }
-        if (quantia > 20){
-            qtdVint = 0;
-            while (quantia > 20) {
-                quantia -= 20;
-                qtdVint++;
-            }
+    }
+    if (quantia >= 20){
+        qtdVint = 0;
+        while (quantia >= 20) {
+            quantia -= 20;
+            qtdVint++;
         }
-        if (quantia > 10){
-            qtdDez = 0;
-            while (quantia > 10) {
-                quantia -= 10;
-                qtdDez++;
-            }
+    }
+    if (quantia >= 10){
+        qtdDez = 0;
+        while (quantia >= 10) {
+            quantia -= 10;
+            qtdDez++;
         }
-        if (quantia > 5){
-            qtdCinc = 0;
-            while (quantia > 5) {
-                quantia -= 5;
-                qtdCinc++;
-            }
+    }
+    if (quantia >= 5){
+        qtdCinc = 0;
+        while (quantia >= 5) {
+            quantia -= 5;
+            qtdCinc++;
         }
-        if (quantia > 2){
-            qtdDois = 0;
-            while (quantia > 2) {
-                quantia -= 2;
-                qtdDois++;
-            }
+    }
+    if (quantia >= 2){
+        qtdDois = 0;
+        while (quantia >= 2) {
+            quantia -= 2;
+            qtdDois++;
         }
-        if (quantia > 1){
-            qtdUm = 0;
-            while (quantia > 1) {
-                quantia -= 1;
-                qtdUm++;
-            }
+    }
+    if (quantia >= 1){
+        qtdUm = 0;
+        while (quantia >= 1) {
+            quantia -= 1;
+            qtdUm++;
         }
     }
     printf("%d notas de 100, %d notas de 50, %d notas de 20, %d notas de 10, %d notas de 5, %d notas de 2 e %d notas de 1.", qtdCem, qtdCinq, qtdVint, qtdDez, qtdCinc, qtdDois, qtdUm);
