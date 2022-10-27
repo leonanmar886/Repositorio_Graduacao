194fd042ed3ea55fd93dd3bece5762c723547b79
leonan <leonan.marques886@gmail.com>
2022-10-27 13:37:16 -0300

    TP04 finalizada

--- Segundo Semestre/Programação/TP04/Pilha.c
+++ Segundo Semestre/Programação/TP04/Pilha.c
@@ -14,11 +14,15 @@ typedef struct node No;
 
 struct pilha {
   No *primeiro;
+  int tamanho;
 };
 
 /* Aloca espaço em memória e retorna uma pilha. */
 Pilha *pilha_cria() {
-  return NULL;
+  Pilha *pilha = (Pilha *)malloc(sizeof(Pilha));
+  pilha->primeiro = NULL;
+  pilha->tamanho = 0;
+  return pilha;
 }
 
 /* Libera a memória de uma pilha previamente criada e atribui NULL ao ponteiro
@@ -45,19 +49,118 @@ int pilha_libera(Pilha **pilha) {
  * adicionado) e -1 caso a pilha ou aluno sejam NULL.
  */
 int pilha_push(Pilha *pilha, Aluno *aluno) {
-  return -2;
+  if (pilha == NULL || aluno == NULL) {
+    return -1;
+  }
+
+  if (pilha->primeiro == NULL) {
+    No *novoNo = (No *)malloc(sizeof(No));
+    novoNo->aluno = aluno;
+    novoNo->proximo = NULL;
+    No **enderecoNoComparado = &(pilha->primeiro);
+    *enderecoNoComparado = novoNo;
+    pilha->tamanho++;
+    return 1;
+  }
+
+  int matFixa;
+  char nomeFixo[50];
+  char cursoFixo[30];
+
+  int matricula;
+  char nome[50];
+  char curso[30];
+
+  alu_acessa(aluno, &matFixa, nomeFixo,
+             cursoFixo); // atribui as variaveis passadas na função, os valores
+                         // dos campos de aluno.
+
+  No *noAtual = pilha->primeiro;
+  No *noAnterior = noAtual;
+
+  while (noAtual !=
+         NULL) { // enquanto o nó apontar para um próximo, ou seja, enquanto o
+                 // nó comparado não seja o último nó da pilha.
+    alu_acessa(noAtual->aluno, &matricula, nome,
+               curso); // acessa os valores do aluno do próximo nó
+
+    if (matricula == matFixa) {
+      return 0;
+    }
+
+    noAnterior = noAtual;
+    noAtual = noAtual->proximo; // pula para o próximo nó.
+  }
+
+  No *novoNo = (No *)malloc(sizeof(No));
+  novoNo->aluno = aluno;
+  novoNo->proximo = NULL;
+  No **enderecoNoComparado = &(noAnterior->proximo);
+  *enderecoNoComparado = novoNo;
+  pilha->tamanho++;
+  return 1;
 }
 
 /* Remove um aluno na pilha. Retorna o aluno ou NULL caso a pilha esteja vazia
  * ou seja NULL. */
 Aluno *pilha_pop(Pilha *pilha) {
-  return NULL;
+  if (pilha == NULL || pilha->primeiro == NULL) {
+    return NULL;
+  }
+  No *noAtual = pilha->primeiro;
+  No *noAnterior = noAtual;
+  No *noAnteriorAnterior = noAtual;
+  while (noAtual != NULL) {
+    noAnteriorAnterior = noAnterior;
+    noAnterior = noAtual;
+    noAtual = noAtual->proximo;
+  }
+  noAnteriorAnterior->proximo = NULL;
+  return noAnterior->aluno;
 }
 
+/* Recupera o primeiro aluno da pilha. Retorna o aluno ou NULL caso a pilha
+ * esteja vazia ou seja NULL. */
+Aluno *pilha_top(Pilha *pilha) {
+  if (pilha == NULL || pilha->primeiro == NULL) {
+    return NULL;
+  }
+  No *noAtual = pilha->primeiro;
+  No *noAnterior = noAtual;
+
+  while (noAtual != NULL) { // enquanto o nó apontar para um próximo, ou seja, enquanto o
+                 // nó comparado não seja o último nó da pilha.
+    noAnterior = noAtual;
+    noAtual = noAtual->proximo; // pula para o próximo nó.
+  }
+  return noAnterior->aluno;
+}
 /* Busca aluno pelo número de matricula. Retorna o aluno se este estiver na
  * lista e NULL caso contrário, isto é, (i) pilha vazia; (ii) não exista aluno
  * com a matricula fornecida; ou (iii) a pilha seja NULL */
 Aluno *pilha_busca(Pilha *pilha, int matricula) {
+  if (pilha_vazia(pilha) == 1 || pilha == NULL) {
+    return NULL;
+  }
+
+  // criação de variáveis para receberem os valores do aluno criado
+  int matriculaAluno;
+  char nome[50];
+  char curso[30];
+
+  No *noAtual = pilha->primeiro;
+  No *noAux = noAtual;
+
+  while (noAtual != NULL) {
+    alu_acessa(noAtual->aluno, &matriculaAluno, nome, curso); // acessando os dados de cada aluno de cada nó visitado
+    if (matriculaAluno ==
+        matricula) { // verifica se há matrícula igual a do aluno novo
+      return noAtual->aluno;
+    }
+    noAux = noAtual;
+    noAtual = noAtual->proximo;
+  }
+
   return NULL;
 }
 
@@ -77,6 +180,4 @@ int pilha_vazia(Pilha *pilha) {
 /* Computa a quantidade de alunos na pilha. Retorna a quantidade de alunos
  * ou -1, caso a Pilha for NULL.
  */
-int pilha_quantidade(Pilha *pilha) {
-  return -2;
-}
+int pilha_quantidade(Pilha *pilha) { return pilha->tamanho; }
