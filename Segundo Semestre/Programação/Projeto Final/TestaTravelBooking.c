e0fcf8985eeb7980adb943007aed29ab29f99c67
leonan <leonan.marques886@gmail.com>
2022-11-12 12:02:39 -0300

    Teste da criação do passageiro implementado.

--- Segundo Semestre/Programação/Projeto Final/TestaTravelBooking.c
+++ Segundo Semestre/Programação/Projeto Final/TestaTravelBooking.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int teste_passageiro_novo(){
+  char nome1[50] = "Carlinhos";
+  char endereco1[100] = "Rua Taquari, 3850";
+
+  char nome2[50] = "89849384"
+  char endereco2[100] = "75834578"
+
+  Passageiro* passageiro1 = passageiro_novo(1, &nome1, &endereco1);
+  Passageiro* passageiro2 = passageiro_novo(-1, &nome2, &endereco2);
+  Passageiro* passageiro3 = passageiro_novo(1, NULL, NULL);
+
+
+}
 
 int main(void) {
   printf("Hello World, Equipe\n");
